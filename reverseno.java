14695412e9c03eb81340195d01a946a33290c02b
VasukiA <vasuki257@gmail.com>
2016-08-16 18:38:24 +0530

    Create reverseno.java

---
+++ reverseno.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class reverseno
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+while(n>0)
+{
+int x=n%10;
+System.out.print(x);
+n=n/10;
+}
+}
+}
