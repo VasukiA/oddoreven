40e3614583f9d3e85b39e04c4d9c0d05162adbb9
VasukiA <vasuki257@gmail.com>
2016-08-16 18:41:52 +0530

    Update reverseno.java

--- reverseno.java
+++ reverseno.java
@@ -6,11 +6,19 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
+if(n>0)
+{
 while(n>0)
 {
 int x=n%10;
 System.out.print(x);
 n=n/10;
+}
+}
+else
+{
+  System.out.println("negative no")
+}
 }
 }
 }
