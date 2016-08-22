80ff90a894ed9b9e079352ace3e010e8bb3ece7e
VasukiA <vasuki257@gmail.com>
2016-08-22 18:31:55 +0530

    Create player6.java

---
+++ player6.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class player6
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.next();
+String s2=sc.next();
+String out="";
+if(s1.length()==s2.length())
+{
+for(int i=0;i<s1.length();i++)
+{
+out=s1.replace(s1.charAt(i),s2.charAt(i));
+s1=out;
+}
+if(out.equals(s2)
+{
+System.out.priintln(true);
+}
+}
+else
+{
+System.out.println(false);
+}
+}
+}
