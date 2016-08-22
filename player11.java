4111287858e3082493772d5a79446c89b1fa9013
VasukiA <vasuki257@gmail.com>
2016-08-22 18:59:09 +0530

    Create player11.java

---
+++ player11.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class player11
+{
+public static void main(String args[])
+{
+String day=new Scanner(System.in).next();
+if(day.equalsIgnoreCase("sunday")
+{
+System.out.println(false);
+}
+else
+{
+System.out.println(true);
+}
+}
+}
