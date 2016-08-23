46624a9ae0174408391b3f52030636e3b03d3816
VasukiA <vasuki257@gmail.com>
2016-08-23 18:58:36 +0530

    Create player51.java

---
+++ player51.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class player51
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+for(int j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+int t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+System.out.println(a[1]);
+}
+}
