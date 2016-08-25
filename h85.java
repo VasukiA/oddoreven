14732fd682e3cdee1a2888100603c5bfcd25e5fc
VasukiA <vasuki257@gmail.com>
2016-08-25 10:20:18 +0530

    Create h85.java

---
+++ h85.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class h85
+{
+public static void main(String args[])
+{
+int n=new Scanner(System.in).nextInt();
+int count=n/2;
+/*while(n>0)
+{
+n=n/2;
+count++;
+}*/
+System.out.println(count);
+}
+}
