09e872550c09c8a4885007682a902590ed700e24
VasukiA <vasuki257@gmail.com>
2016-08-28 15:41:33 +0530

    Create b9.java

---
+++ b9.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class b9
+{
+public static void main(String args[])
+{
+int sum=0;
+int n=new Scanner(System.in).nextInt();
+for(int i=1;i<=n;i++)
+{
+sum+=i;
+}
+System.out.println(sum);
+}
+}
