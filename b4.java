182b6a760b35384c35d9054946a33d1383c6ea50
VasukiA <vasuki257@gmail.com>
2016-08-28 11:33:24 +0530

    Create b4.java

---
+++ b4.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class b4
+{
+public static void main(String args[])
+{
+char c=new Scanner(System.in).next().charAt(0);
+if(Character.isAlphabetic(c))
+System.out.println("Alphabet");
+else
+System.out.println("not an Alphabet");
+}
+}
