13d6a961304b73f626b1e1a17e8d97b8d51ae0f1
VasukiA <vasuki257@gmail.com>
2016-08-28 11:28:50 +0530

    Create b3.java

---
+++ b3.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class b3
+{
+public static void main(String args[])
+{
+char c=new Scanner(System.in).next().charAt(0);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+System.out.println("Vowel");
+else
+System.out.println("Consonant");
+}
+}
