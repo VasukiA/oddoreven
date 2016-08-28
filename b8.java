2b13a61c4416f177cd2085b5aa7a104509ca6b8c
VasukiA <vasuki257@gmail.com>
2016-08-28 15:35:33 +0530

    Create b8.java

---
+++ b8.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class b8
+{
+public static void main(String args[])
+{
+int n=new Scanner(System.in).nextInt();
+int ans=(n+1)/2;
+System.out.println(n*ans);
+}
+}
