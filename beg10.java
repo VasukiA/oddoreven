7bc96c92925f83e05efd77a8e5428cdcac8fa36b
VasukiA <vasuki257@gmail.com>
2016-10-08 10:45:37 +0530

    Rename b10.java to beg10.java

---
+++ beg10.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class b10
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+System.out.println(String.valueOf(n).length());
+}
+}
