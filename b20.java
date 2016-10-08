b55f91bcb9c87476a69b8dc669e7c0c70ef6e1e9
VasukiA <vasuki257@gmail.com>
2016-10-08 11:17:09 +0530

    Create b20.java

---
+++ b20.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class b20
+{
+public static void main(String args[])
+{
+int n=new Scanner(System.in).nextInt();
+for(int i=1;i<16;i++)
+{
+System.out.println(i*n);
+}
+}
+}
